3d5b35119b007d1dc7f9ce2b3fe2c11ad4cf1bec
Ssiyam0123 <ssiyam563@gmail.com>
2025-05-09 20:12:38 +0600

    make the home card section

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,36 +1,33 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import MobileNav from './MobileNav'
-import { SignedIn, UserButton } from '@clerk/nextjs'
-import { auth } from '@clerk/nextjs/server'
+import Image from 'next/image';
+import Link from 'next/link';
+import { SignedIn, UserButton } from '@clerk/nextjs';
+
+import MobileNav from './MobileNav';
 
 const Navbar = () => {
-  const{user}= auth()
-  console.log(user)
   return (
-    <nav className='flex-between fixed z-50 w-full bg-black px-6 lg:px-10 bg-dark-1 mt-7'>
-      <Link href={'/'} className='flex items-center gap-1'>
-        <Image src={'/icons/logo.svg'}
-        width={32}
-        height={32}
-        alt='yoom logo'
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          width={32}
+          height={32}
+          alt="yoom logo"
+          className="max-sm:size-10"
         />
-        <p className='text-[26px] text-white font-extrabold max-sm:hidden'>Yoom</p>
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          YOOM
+        </p>
       </Link>
-      <div className='flex-between gap-5'>
-        {/* clerk management */}
-        <div className='flex-between gap-5'>
-          <SignedIn>
-            <UserButton/>
-          </SignedIn>
+      <div className="flex-between gap-5">
+        <SignedIn>
+          <UserButton afterSignOutUrl="/sign-in" />
+        </SignedIn>
 
-        </div>
-        <MobileNav/>
-
+        <MobileNav />
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
