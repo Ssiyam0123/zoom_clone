a84dc8ef7e0d709f03a0fb7ffa9a66ee4ad24b4f
Ssiyam0123 <ssiyam563@gmail.com>
2025-05-12 17:38:01 +0600

    almighty pushhhhhh

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -3,25 +3,21 @@ import Sidebar from "@/app/components/Sidebar";
 import { Metadata } from "next";
 import React, { ReactNode } from "react";
 
-
-
 export const metadata: Metadata = {
-  title: 'YOOM',
-  description: 'A workspace for your team, powered by Stream Chat and Clerk.',
+  title: "YOOM",
+  description: "A workspace for your team, powered by Stream Chat and Clerk.",
 };
-
 
 const HomeLayout = ({ children }: { children: ReactNode }) => {
   return (
     <main className="relative">
-      <Navbar/>
+      <Navbar />
       <div className="flex">
-        <Sidebar/>
+        <Sidebar />
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:scroll-px-14">
           <div className="w-full">{children}</div>
         </section>
       </div>
-      footer
     </main>
   );
 };
